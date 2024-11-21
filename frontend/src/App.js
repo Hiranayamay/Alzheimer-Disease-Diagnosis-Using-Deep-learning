19978cab053ddd2351789f9f9c4f6c16384e4d5c
Hiranayamay <107201577+Hiranayamay@users.noreply.github.com>
2024-11-22 00:59:58 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UploadImage from './components/UploadImage';
+
+
+function App() {
+    return (
+        <div className="App">
+            <UploadImage />
+        </div>
+    );
+}
+
+export default App;
